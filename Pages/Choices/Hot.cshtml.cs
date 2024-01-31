ed8e61d4450ab7a92ec38fcbf2faaba8e7a03d05
zednik-matej <zednik.mattej@gmail.com>
2024-01-31 21:44:28 +0100

    Choices adding and removing handlers

--- Pages/Choices/Hot.cshtml.cs
+++ Pages/Choices/Hot.cshtml.cs
@@ -1,8 +1,10 @@
 using Jopp_lunch.Model.DbEntities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using System.Security.Claims;
 
 namespace Jopp_lunch.Pages.Choices
 {
@@ -10,9 +12,13 @@ namespace Jopp_lunch.Pages.Choices
     {
         private readonly Jopp_lunch.Data.CanteenContext _context;
 
-        public HotModel(Jopp_lunch.Data.CanteenContext context)
+        private readonly UserManager<User> _userManager;
+
+        public HotModel(Jopp_lunch.Data.CanteenContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
+            
         }
 
         DateTime startOfWeek;
@@ -20,11 +26,14 @@ namespace Jopp_lunch.Pages.Choices
 
         public IList<Soup> Soup { get; set; } = default!;
         public IList<Lunch> Lunch { get; set; } = default!;
+        public IList<Choice> Choices { get; set; } = default!;
+
 
         public string Thisweek = "";
+
         public async Task OnGetAsync()
         {
-            if(DateTime.Now.Hour>=12) startOfWeek = DateTime.Today.AddDays(2);
+            if (DateTime.Now.Hour>=12) startOfWeek = DateTime.Today.AddDays(2);
             else startOfWeek = DateTime.Today.AddDays(1);
             endOfWeek = DateTime.Today.AddDays(
             (int)CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek + 4 -
@@ -34,16 +43,122 @@ namespace Jopp_lunch.Pages.Choices
             if (_context.polevky != null)
             {
                 Soup = await _context.polevky
-                    .Where(x=> x.datum_vydeje.Date>=startOfWeek.Date && x.datum_vydeje.Date<=endOfWeek.Date)
+                    .Where(x=> x.datum_vydeje.Date>=startOfWeek.Date /*&& x.datum_vydeje.Date<=endOfWeek.Date*/)
                     .ToListAsync();
             }
             if (_context.obedy != null)
             {
                 Lunch = await _context.obedy
-                    .Where(x => x.datum_vydeje.Date >= startOfWeek.Date && x.datum_vydeje.Date <= endOfWeek.Date)
+                    .Where(x => x.datum_vydeje.Date >= startOfWeek.Date /*&& x.datum_vydeje.Date <= endOfWeek.Date*/)
                     .ToListAsync();
-            }
+                if (_context.vybery != null && _context.uzivatele != null)
+                {
+                    User usr = _context.uzivatele.Where(x => x.Id == _userManager.GetUserId(HttpContext.User)).FirstOrDefault();
+                    Choices = await _context.vybery
+                        .Where(x => Lunch.Contains(x.obedId) && x.cislo_uzivatele== usr /*&& x.datum_vydeje.Date <= endOfWeek.Date*/)
+                        .ToListAsync();
+                }
+            }          
+        }
 
+
+
+        public IActionResult OnGetAddLunch(int id)
+        {
+            if (_context.vybery != null && _context.obedy != null) {
+                Lunch lnch = _context.obedy.Where(ob => ob.cislo_obeda==id).FirstOrDefault();
+                if (lnch != null)
+                {
+                    Choice choice = _context.vybery.Where(x => x.obedId == lnch).FirstOrDefault();
+                    if (choice != null)
+                    {
+                        choice.pocet = choice.pocet + 1;
+                        _context.vybery.Update(choice);
+                        _context.SaveChanges();
+                    }
+                    else
+                    {
+                        if (_context.uzivatele != null)
+                        {
+                            choice = new Choice();
+                            choice.pocet += 1;
+                            choice.cislo_uzivatele = _context.uzivatele.Where(x => x.Id == _userManager.GetUserId(HttpContext.User)).FirstOrDefault();
+                            choice.vydejni_misto = _context.vydejni_mista.Where(x => x.cislo_VM == 1).FirstOrDefault();
+                            choice.obedId = lnch;
+                            _context.vybery.Add(choice);
+                            _context.SaveChanges();
+                        }
+                    }
+                }
+            }
+            if (DateTime.Now.Hour >= 12) startOfWeek = DateTime.Today.AddDays(2);
+            else startOfWeek = DateTime.Today.AddDays(1);
+            endOfWeek = DateTime.Today.AddDays(
+            (int)CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek + 4 -
+            (int)DateTime.Today.DayOfWeek);
+            if (_context.polevky != null)
+            {
+                Soup =  _context.polevky
+                    .Where(x => x.datum_vydeje.Date >= startOfWeek.Date /*&& x.datum_vydeje.Date<=endOfWeek.Date*/)
+                    .ToList();
+            }
+            if (_context.obedy != null)
+            {
+                Lunch =  _context.obedy
+                    .Where(x => x.datum_vydeje.Date >= startOfWeek.Date /*&& x.datum_vydeje.Date <= endOfWeek.Date*/)
+                    .ToList();
+                if (_context.vybery != null && _context.uzivatele != null)
+                {
+                    User usr = _context.uzivatele.Where(x => x.Id == _userManager.GetUserId(HttpContext.User)).FirstOrDefault();
+                    Choices =  _context.vybery
+                        .Where(x => Lunch.Contains(x.obedId) && x.cislo_uzivatele == usr /*&& x.datum_vydeje.Date <= endOfWeek.Date*/)
+                        .ToList();
+                }
+            }
+            return Page();
+        }
+
+        public IActionResult OnGetRemoveLunch(int id)
+        {
+            if (_context.vybery != null && _context.obedy != null)
+            {
+                Lunch lnch = _context.obedy.Where(ob => ob.cislo_obeda == id).FirstOrDefault();
+                if (lnch != null)
+                {
+                    Choice choice = _context.vybery.Where(x => x.obedId == lnch).FirstOrDefault();
+                    if (choice != null && choice.pocet>0)
+                    {
+                        choice.pocet--;
+                        _context.vybery.Update(choice);
+                        _context.SaveChanges();
+                    }
+                }
+            }
+            if (DateTime.Now.Hour >= 12) startOfWeek = DateTime.Today.AddDays(2);
+            else startOfWeek = DateTime.Today.AddDays(1);
+            endOfWeek = DateTime.Today.AddDays(
+            (int)CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek + 4 -
+            (int)DateTime.Today.DayOfWeek);
+            if (_context.polevky != null)
+            {
+                Soup =  _context.polevky
+                    .Where(x => x.datum_vydeje.Date >= startOfWeek.Date /*&& x.datum_vydeje.Date<=endOfWeek.Date*/)
+                    .ToList();
+            }
+            if (_context.obedy != null)
+            {
+                Lunch =  _context.obedy
+                    .Where(x => x.datum_vydeje.Date >= startOfWeek.Date /*&& x.datum_vydeje.Date <= endOfWeek.Date*/)
+                    .ToList();
+                if (_context.vybery != null && _context.uzivatele != null)
+                {
+                    User usr = _context.uzivatele.Where(x => x.Id == _userManager.GetUserId(HttpContext.User)).FirstOrDefault();
+                    Choices =  _context.vybery
+                        .Where(x => Lunch.Contains(x.obedId) && x.cislo_uzivatele == usr /*&& x.datum_vydeje.Date <= endOfWeek.Date*/)
+                        .ToList();
+                }
+            }
+            return Page();
         }
     }
 }
---
+++ Pages/Choices/List.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Jopp_lunch.Data;
+using Jopp_lunch.Model.DbEntities;
+
+namespace Jopp_lunch.Pages.Choices
+{
+    public class ListModel : PageModel
+    {
+        private readonly Jopp_lunch.Data.CanteenContext _context;
+
+        public ListModel(Jopp_lunch.Data.CanteenContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Choice> Choice { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.vybery != null)
+            {
+                Choice = await _context.vybery.ToListAsync();
+            }
+        }
+    }
+}
